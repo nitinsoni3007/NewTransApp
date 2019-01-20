b60de513f99f28e4e824ad86938d734fb6810d3b
Snehil <Snehil@Nitins-MacBook-Pro.local>
2019-01-20 12:21:08 +0530

    Added react-navigation

--- TransApp/index.js
+++ TransApp/index.js
@@ -3,5 +3,18 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import HomeScreen from './Home';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppNavigator = createStackNavigator({
+    App: {
+        screen: App
+    },
+    Home: {
+        screen: HomeScreen
+    }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);
